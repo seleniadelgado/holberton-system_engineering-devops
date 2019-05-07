4666a6e341ffaf7c8b1fce9529a753c87c563707
--get <angiedelgado08@gmail.com>
2019-05-07 06:21:46 +0000

    added if condition

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""exports python script to export data in the JSON format]"""
+"""exports python script to export data in the JSON format"""
 from sys import argv
 import json
 import requests
 if __name__ == "__main__":
 
     url = requests.get('https://jsonplaceholder.typicode.com/todos')
-
     todo_list = url.json()
 
     usrs = requests.get('https://jsonplaceholder.typicode.com/users')
@@ -15,16 +14,17 @@ if __name__ == "__main__":
     json_file = "todo_all_employees.json"
 
     data = {}
-    data_list = []
+    todo_task_list = []
     for user in usrs:
         USERNAME = user.get('username')
         USER_ID = user.get('id')
         for task in todo_list:
-            little_dict = {}
-            little_dict["task"] = task.get('title')
-            little_dict["completed"] = task.get('completed')
-            little_dict["username"] = USERNAME
-            data_list.append(little_dict)
-        data[USER_ID] = data_list
+            if task['userId'] is USER_ID:
+                task_dict = {}
+                task_dict["task"] = task.get('title')
+                task_dict["completed"] = task.get('completed')
+                task_dict["username"] = USERNAME
+                todo_task_list.append(task_dict)
+            data[USER_ID] = todo_task_list
     with open(json_file, 'w') as j:
         json.dump(data, j)
