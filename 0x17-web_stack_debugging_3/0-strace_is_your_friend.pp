33abf8785aab7dfa30d6b8f3d5686caa963a49f7
--get <angiedelgado08@gmail.com>
2019-05-23 03:27:07 +0000

    removed restart

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #create a script that replaces a misspelling in a file.
 exec { 'sed':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php sudo service Apache restart",
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
 }
