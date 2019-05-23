f64f6319e7b35224641f88adc02977b3d9f1cde4
--get <angiedelgado08@gmail.com>
2019-05-23 03:20:31 +0000

    adding restart for server

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #create a script that replaces a misspelling in a file.
 exec { 'sed':
-     command => 'sed -i 's/phpp/php/g' wp-settings.php
-     path    => ['/var/www/html']
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php sudo service Apache restart",
+  path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
 }
