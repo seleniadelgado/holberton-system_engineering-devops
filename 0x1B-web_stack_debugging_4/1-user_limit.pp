bc343f778080036cc70dfcbbad40e4a956c6d0fc
Selenia Delgado <37052040+seleniadelgado@users.noreply.github.com>
2019-05-31 00:09:27 -0700

    Delete 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,5 +0,0 @@
-# This file fixes the Nginx server
-exec { 'change_user_limit':
-command => "sed -i 's/5/6000/g; s/4/8000/g' /etc/security/limits.conf",
-path    => [ '/bin/', '/sbin/', '/usr/bin', '/usr/sbin/' ]
-}
