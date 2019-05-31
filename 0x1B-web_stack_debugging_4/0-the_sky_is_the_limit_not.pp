25a295c5d45a7f5cb7f940a023c488211364534d
seleniadelgado <angiedelgado08@gmail.com>
2019-05-31 06:55:04 +0000

    changed it to a higher number of requests

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # a script that increases the limit of requests.
 exec { 'limit_request_increase':
-  command => "sed -i 's/15/3000/g' /etc/default/nginx; service nginx restart",
+  command => "sed -i 's/15/25000/g' /etc/default/nginx; service nginx restart",
   path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/']
 }
